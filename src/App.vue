cff45541cf70c34e63cf5971fcca12d50550bc44
thatsShirleyLee <2690233316@qq.com>
2024-07-28 17:00:51 +0800

    feat: add api manager

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,6 @@
 <script setup lang="ts">
-import request from '@/utils/request'
-import { onMounted } from 'vue'
-onMounted(() => {
-  request({
-    url: '/user/login',
-    method: 'post',
-    data: {
-      username: 'admin',
-      password: '111111',
-    },
-  }).then((res) => {
-    console.log(res)
-  })
-})
+  import { reqLogin, reqUserInfo } from '@/api/user/user'
+  reqLogin({username: 'admin', password: '111111'})
 </script>
 
 <template>
---
+++ src/api/user/user.ts
@@ -0,0 +1,16 @@
+// 统一管理项目用户相关的接口
+import request from '@/utils/request'
+import type { loginForm, loginResponseData, userInfoResponseData } from './type'
+// 统一管理接口
+enum API {
+  LOGIN_URL = '/user/login',
+  USERINFOR_URL = '/user/info',
+}
+// 暴露请求函数
+
+// 登录接口方法
+export const reqLogin = (data: LoginForm) =>
+  request.post<any, loginResponseData>(API.LOGIN_URL, data)  // loginResponseData example{ code: 200, data: { token } }
+// 获取用户信息接口方法
+export const reqUserInfo = () =>
+  request.get<any, userInfoResponseData>(API.USERINFOR_URL)  // userInfoResponseData example { code: 200, data: { checkUser } }
