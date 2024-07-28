8b76ac40496a56957b5e1d05ad4c0fb52de579d2
thatsShirleyLee <2690233316@qq.com>
2024-07-29 01:03:35 +0800

    feat: init login view

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,60 @@
-<script setup lang="ts" name="LoginView"></script>
+<script setup lang="ts" name="LoginView">
+import { User, Lock } from '@element-plus/icons-vue'
+</script>
 
 <template>
-  <h1>一级路由login</h1>
+  <div class="login_container">
+    <el-row>
+      <el-col :span="12" :xs="0"></el-col>
+      <el-col :span="12" :xs="24">
+        <el-form class="login_form">
+          <h1>Hello</h1>
+          <h2>欢迎来到硅谷甄选</h2>
+          <el-form-item>
+            <el-input :prefix-icon="User"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input :prefix-icon="Lock" type="password"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button class="login_btn" type="primary" size="default">
+              登录
+            </el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.login_container {
+  width: 100%;
+  height: 100vh;
+  background: url('@/assets/images/background.jpg') no-repeat center center;
+  background-size: cover;
+
+  .login_form {
+    position: relative;
+    width: 80%;
+    top: 30vh;
+    background: url('@/assets/images/login_form.png') no-repeat;
+    background-size: cover;
+    padding: 40px;
+    border-radius: 20px;
+    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
+    h1 {
+      color: white;
+      font-size: 40px;
+    }
+    h2 {
+      color: white;
+      font-size: 20px;
+      margin: 20px 0px;
+    }
+    .login_btn {
+      width: 100%;
+    }
+  }
+}
+</style>
